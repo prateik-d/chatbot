061ad01fa4a81018c628a36fda4b4e763e22671a
Prateik <pratik.dhotmal@gmail.com>
2020-02-04 12:12:57 +0530

    add slider images from dialogFlow

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ChatDialogComponent } from './chat-dialog/chat-dialog.component';
 import { ChatService } from './chat.service';
+import { SafeHtmlPipePipe } from './chat-dialog/safe-html-pipe.pipe';
 
 @NgModule({
   imports: [CommonModule, FormsModule],
-  declarations: [ChatDialogComponent],
+  declarations: [ChatDialogComponent, SafeHtmlPipePipe],
   exports: [ChatDialogComponent],
   providers: [ChatService]
 })
